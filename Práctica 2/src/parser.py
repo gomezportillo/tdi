f573b1c2e4b4837bfc13df6befe61e6d17479d38
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2018-11-15 11:22:29 +0100

    parser fixed

--- Práctica 2/src/parser.py
+++ Práctica 2/src/parser.py
@@ -4,8 +4,17 @@ import os
 apps_file_array = ['..', 'data', 'google-play-store-apps', 'googleplaystore.csv']
 reviews_file_array = ['..', 'data', 'google-play-store-apps', 'googleplaystore_user_reviews.csv']
 
-apps_file = os.path.join(*apps_file_array)
-reviews_file = os.path.join(*reviews_file_array)
+# Define test files
+test_apps_file_array = ['tests', 'apps.csv']
+test_reviews_file_array = ['tests', 'reviews.csv']
+
+TESTING = False
+if TESTING:
+    apps_file = os.path.join(*test_apps_file_array)
+    reviews_file = os.path.join(*test_reviews_file_array)
+else:
+    apps_file = os.path.join(*apps_file_array)
+    reviews_file = os.path.join(*reviews_file_array)
 
 print("Apps file: {}".format(apps_file))
 print("Reviews file: {}".format(reviews_file))
@@ -17,8 +26,9 @@ appsDict = {}
 APP_HEADER = ''
 if os.path.isfile(apps_file):
     with open(apps_file, 'r', encoding="utf8") as fileApps:
-        APP_HEADER = fileApps.readline()
+        APP_HEADER = fileApps.readline().replace('\n', '')
         for line in fileApps:
+            line = line.replace('\n', '')
             tokens = line.split(',')
             appsDict [tokens[0] ] = tokens[1:] #app name : list with rest of info
 
@@ -30,20 +40,21 @@ else:
 reviewDict = {}
 MAX_NUMBER_OF_REVIEWS = 5
 if os.path.isfile(reviews_file):
-    with open(apps_file, 'r', encoding="utf8") as fileRev:
+    with open(reviews_file, 'r', encoding="utf8") as fileRev:
         fileRev.readline() # avoid first line
         for line in fileRev:
+            line = line.replace('\n', '')
             tokens = line.split(',')
             if tokens[0] not in reviewDict:
                 reviewDict[ tokens[0] ] = []
-                reviewDict[ tokens[0] ].append( tokens[0] )
+                reviewDict[ tokens[0] ].append( tokens[1] )
             else:
                 if len(reviewDict[ tokens[0] ]) <= MAX_NUMBER_OF_REVIEWS:
-                    reviewDict[ tokens[0] ].append( tokens[0] )
+                    reviewDict[ tokens[0] ].append( tokens[1] )
                 else:
                     pass # la review se descarta
 
-    print("Reviews found {}".format( len(reviewDict) ))
+    print("Apps with reviews found {}".format( len(reviewDict) ))
 else:
     print("Can't find file {}".format(apps_file))
 
@@ -52,6 +63,11 @@ else:
 # Adding reviews to app and saving to file
 out_file_name = ['out', 'parsed_apps.csv']
 out_file = os.path.join(*out_file_name)
+
+for x in range(MAX_NUMBER_OF_REVIEWS):
+    APP_HEADER += ',Comments'
+APP_HEADER += '\n'
+
 with open(out_file, 'w', encoding="utf8") as fout:
     fout.write(APP_HEADER)
     for app in appsDict:
@@ -62,7 +78,6 @@ with open(out_file, 'w', encoding="utf8") as fout:
             line += reviewDict[app]
 
             line_with_commas = ",".join( [str(x) for x in line] )
-            print(line_with_commas)
-            fout.write(line_with_commas)
+            fout.write(line_with_commas + '\n')
         else:
             pass #if no comments in app avoid writing it
